9867a19cb14e43a414b2151f64e1893debfce2bc
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2021-11-09 17:36:44 +0300

    test1

---
+++ NetworkChat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetworkChat
+{
+    class Program
+    {
+        private Client Client;
+        private Server Server;
+        
+        private async static Task Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var client = new Client(args[0], int.Parse(args[1]));
+                client.Go();
+            }
+            else
+            {
+                var listener = new Server(int.Parse(args[0]));
+                listener.Go();
+            }
+        }
+        
+    }
+
+}    
