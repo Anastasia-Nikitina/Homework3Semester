2e4af594b9f143ea51d78d601b8de00af385e557
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-09-05 20:01:04 +0300

    .

--- MyFTP/MyFTPTests/Tests.cs
+++ MyFTP/MyFTPTests/Tests.cs
@@ -64,10 +64,11 @@ public class Tests
     public async Task TestForGet()
     {
         var path = "../../../Test/Directory";
-        var destination = path + "/fileForCopy.txt";
-        var pathForGet = path + "/file1.txt";
+        var destination = Path.Combine(path, "/fileForCopy.txt");
+        var pathForGet = Path.Combine(path, "/file1.txt");
         var expect = await File.ReadAllBytesAsync(pathForGet, _token);
-        await using var fileStream = new FileStream(destination, FileMode.OpenOrCreate); await _client.Get(pathForGet, fileStream, _token);
+        await using var fileStream = new FileStream(destination, FileMode.OpenOrCreate); 
+        await _client.Get(pathForGet, fileStream, _token);
         var result = await File.ReadAllBytesAsync(destination, _token);
         Assert.AreEqual(expect, result);
     }
