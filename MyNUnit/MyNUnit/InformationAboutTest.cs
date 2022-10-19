4cb0be991fcb4f2b916761976e90f65d17407708
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-10-20 01:48:20 +0300

    added comments

--- MyNUnit/MyNUnit/InformationAboutTest.cs
+++ MyNUnit/MyNUnit/InformationAboutTest.cs
@@ -5,9 +5,24 @@ namespace MyNUnit;
 /// </summary>
 public class InformationAboutTest
 {
+    /// <summary>
+    /// The method that returns name of test
+    /// </summary>
     public string Name { get; }
+    
+    /// <summary>
+    /// The method that returns result of test
+    /// </summary>
     public string Result { get; }
+    
+    /// <summary>
+    /// The method that returns the test execution time
+    /// </summary>
     public float Time { get; }
+    
+    /// <summary>
+    /// The method that returns reason of ignore
+    /// </summary>
     public string ReasonOfIgnore { get; }
 
     public InformationAboutTest(string name, string result,  float time, string reasonOfIgnore)
